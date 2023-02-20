f0cf4bc53b4d7137c4af71094958bb4e5cef020f
Vitor-M <vitorm2000@outlook.com>
2023-02-20 18:28:42 -0300

    feat: add room route

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Main from '@/views/Main.vue'
+import Room from '@/views/Room.vue'
 import UserProfile from '@/views/UserProfile.vue'
 
 const routes = [
@@ -7,6 +8,12 @@ const routes = [
         name: 'main',
         path: '/',
         component: Main,
+    },
+    {
+        name: 'room',
+        path: '/room/:roomId',
+        component: Room,
+        props: true
     },
     {
         name: 'profile',
