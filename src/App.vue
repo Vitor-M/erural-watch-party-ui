4e3f996fac9296cc1fdcc98a6341a366afe8e42d
Vitor-M <vitorm2000@outlook.com>
2023-02-20 18:16:55 -0300

    init: project initial components

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container grid-lg">
+    <header class="navbar p-2 s-rounded">
+      <section class="navbar-section">
+        <router-link to="" class="btn btn-link">Home</router-link>
+        <router-link to="" class="btn btn-link">Profile</router-link>
+      </section>
+      <section class="navbar-center">
+        <h1 class="text-bold">Watch🎈Party</h1>
+      </section>
+      <section class="navbar-section">
+        <a
+          href="https://github.com/Vitor-M/erural-watch-party-ui"
+          class="btn btn-link"
+          >GitHub</a
+        >
+      </section>
+    </header>
+    <router-view class="pt-2" />
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 2em;
+  margin: 0;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  margin: 1rem auto;
+  flex: 1;
+}
+</style>
